18194e97a4ef1ee635a9ad59e53a236fc8ae4a66
Anton Kurilov <anton0kurilov@gmail.com>
2023-10-07 11:32:30 +0700

    feat: introduce the header and many aside improvements

--- src/scripts/components/themeToggle.js
+++ src/scripts/components/themeToggle.js
@@ -1,11 +1,17 @@
 const themeToggleElement = document.querySelector('[data-theme-toggle]')
+let htmlElement = document.querySelector('html')
+
+htmlElement.setAttribute('data-theme', localStorage.getItem('theme'))
 
 themeToggleElement.addEventListener('click', function () {
-    let htmlElement = document.querySelector('html'),
-        themeStatus = htmlElement.getAttribute('data-theme')
-    if (themeStatus === 'dark') {
+    if (localStorage.getItem('theme') === 'dark') {
         htmlElement.setAttribute('data-theme', 'light')
-    } else if (themeStatus === 'light') {
+        localStorage.setItem('theme', 'light')
+    } else if (localStorage.getItem('theme') === 'light') {
         htmlElement.setAttribute('data-theme', 'dark')
+        localStorage.setItem('theme', 'dark')
+    } else {
+        htmlElement.setAttribute('data-theme', 'light')
+        localStorage.setItem('theme', 'light')
     }
 })
