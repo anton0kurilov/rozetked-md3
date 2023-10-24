4abca714095d260e41c9cde1c22bb8c40e6a6f5a
Anton Kurilov <anton0kurilov@gmail.com>
2023-10-24 20:20:12 +0700

    fix: now header themeToggle is works

--- src/scripts/components/themeToggle.js
+++ src/scripts/components/themeToggle.js
@@ -1,17 +1,19 @@
-const themeToggleElement = document.querySelector('[data-theme-toggle]')
+const themeToggleElement = document.querySelectorAll('[data-theme-toggle]')
 let htmlElement = document.querySelector('html')
 
 htmlElement.setAttribute('data-theme', localStorage.getItem('theme'))
 
-themeToggleElement.addEventListener('click', function () {
-    if (localStorage.getItem('theme') === 'dark') {
-        htmlElement.setAttribute('data-theme', 'light')
-        localStorage.setItem('theme', 'light')
-    } else if (localStorage.getItem('theme') === 'light') {
-        htmlElement.setAttribute('data-theme', 'dark')
-        localStorage.setItem('theme', 'dark')
-    } else {
-        htmlElement.setAttribute('data-theme', 'dark')
-        localStorage.setItem('theme', 'dark')
-    }
+themeToggleElement.forEach(function (element) {
+    element.addEventListener('click', function () {
+        if (localStorage.getItem('theme') === 'dark') {
+            htmlElement.setAttribute('data-theme', 'light')
+            localStorage.setItem('theme', 'light')
+        } else if (localStorage.getItem('theme') === 'light') {
+            htmlElement.setAttribute('data-theme', 'dark')
+            localStorage.setItem('theme', 'dark')
+        } else {
+            htmlElement.setAttribute('data-theme', 'dark')
+            localStorage.setItem('theme', 'dark')
+        }
+    })
 })
