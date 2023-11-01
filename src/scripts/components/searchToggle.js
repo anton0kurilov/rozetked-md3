dae5dc509c43c642cfbd05a1e25b369e42eb55e4
Anton Kurilov <anton0kurilov@gmail.com>
2023-11-01 11:45:39 +0700

    fix: try to fixing inline script for search [2]

--- src/scripts/components/searchToggle.js
+++ src/scripts/components/searchToggle.js
@@ -1,5 +1,6 @@
 const searchToggleElement = document.querySelector('[data-search-toggle]'),
-    searchInputElement = document.querySelector('.header__search-input')
+    searchInputElement = document.querySelector('.header__search-input'),
+    searchFormElement = document.querySelector('.header__search')
 
 searchToggleElement.addEventListener('click', function () {
     let overlayElement = document.createElement('div')
@@ -12,4 +13,13 @@ searchToggleElement.addEventListener('click', function () {
         overlayAddedElement.remove()
         searchInputElement.classList.remove('active')
     })
+})
+
+searchFormElement.addEventListener('submit', function () {
+    const searchElement = document.querySelector('#search')
+    if (searchElement.value == '') {
+    } else {
+        url = 'https://rozetked.me/search?q=' + searchElement.value
+        window.open(url, '')
+    }
 })
